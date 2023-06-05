cdea98183269be14d97658ad91b85dedbb300551
EMI-INDO <78555833+EMI-INDO@users.noreply.github.com>
2023-06-06 00:31:41 +0700

    billing_version: 6.0.0

--- www/emiInAppPurchase.js
+++ www/emiInAppPurchase.js
@@ -4,12 +4,12 @@ exports.initialize = function (arg0, success, error) {
     exec(success, error, 'emiInAppPurchase', 'initialize', [arg0]);
 };
 
-exports.purchaseNonConsumable = function (arg0, arg1, arg2, arg3, arg4, success, error) {
-    exec(success, error, 'emiInAppPurchase', 'purchaseNonConsumable', [arg0, arg1, arg2, arg3, arg4]);
+exports.purchaseProducts = function (arg0, arg1, arg2, arg3, arg4, success, error) {
+    exec(success, error, 'emiInAppPurchase', 'purchaseProducts', [arg0, arg1, arg2, arg3, arg4]);
 };
 
-exports.restoreNonConsumable = function (arg0, arg1, success, error) {
-    exec(success, error, 'emiInAppPurchase', 'restoreNonConsumable', [arg0, arg1]);
+exports.restorePurchases = function (arg0, arg1, success, error) {
+    exec(success, error, 'emiInAppPurchase', 'restorePurchases', [arg0, arg1]);
 };
 
 exports.getProductDetail = function (arg0, arg1, arg2, success, error) {
